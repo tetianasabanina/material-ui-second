ca03639583e4a4600c30e85477122466f012f0d2
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-11 09:31:02 +0300

    Updates

---
+++ precache-manifest.968d4f3ef39572e6c508b8073050ec90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350880d1e638b10bf9e76da851b06bb6",
+    "url": "/material-ui-second/index.html"
+  },
+  {
+    "revision": "37d64f7f5cd85cf8803e",
+    "url": "/material-ui-second/static/css/main.0cb802b1.chunk.css"
+  },
+  {
+    "revision": "a531c04fa0095c273268",
+    "url": "/material-ui-second/static/js/2.889323df.chunk.js"
+  },
+  {
+    "revision": "37d64f7f5cd85cf8803e",
+    "url": "/material-ui-second/static/js/main.3bfeb522.chunk.js"
+  },
+  {
+    "revision": "b00469c6c08a5329a8f6",
+    "url": "/material-ui-second/static/js/runtime-main.e26c73a2.js"
+  }
+]);
